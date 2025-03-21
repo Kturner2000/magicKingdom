9a7c7c76744968185bfe1b0884f081cd5f0a917b
kylie <ksjturner00@gmail.com>
2025-03-21 12:19:41 +0100

    update server

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,9 +31,9 @@ app.use("/api/articles", articleRoutes);
 
 // Static files and catch-all route (PRODUCTION ONLY)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../../frontend/dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
     });
 }
 
